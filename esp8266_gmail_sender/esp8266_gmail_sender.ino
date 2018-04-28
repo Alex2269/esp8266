373d2ed26700a7d98bc0eb7491fc831c328861e6
Aleksander <alekader@rambler.ru>
2018-04-29 00:28:05 +0300

    Add files via upload

---
+++ esp8266_gmail_sender/esp8266_gmail_sender.ino
@@ -0,0 +1,61 @@
+#include <Gsender.h>
+#include <rBase64.h>
+//This Library and Example is written By: BSIZ1757( Behrooz Sirouszad) ITPro.ir 5.25.2017 b.sirouszad@gmail.com
+//this will send a simple email to your email address
+#include <Gsender.h>
+#include <ESP8266WiFi.h>
+
+Gsender *gsender = Gsender::Instance(); // Getting pointer to class instance
+
+//WiFi Setup
+const char* ssid = "you-ssis";
+const char* password = "you-password";
+
+//Gmail Setup
+int Gamil_smtp_port = 465;
+const char* Gmail_server_address = "smtp.gmail.com";
+
+String Gmail_Login = "xxx123@gmail.com";
+String Gmail_password = "xxxxxx";
+String Gmail_subject = "Your subject";
+
+const char* Gmail_from = "xxx123@gmail.com";
+String Destination_address = "xxx123@gmail.com";
+
+String Message = "esp your Message";
+
+void setup() 
+{
+  Serial.begin(115200);
+  WiFi.begin(ssid,password);
+  while (WiFi.status() != WL_CONNECTED) 
+  {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println(WiFi.localIP());
+  send_gmail();
+}
+
+void loop() 
+{
+}
+
+void send_gmail()
+{
+  gsender->SMTP_PORT=Gamil_smtp_port;
+  gsender->SMTP_SERVER = Gmail_server_address;
+  gsender->EMAILBASE64_LOGIN = rbase64.encode(Gmail_Login);
+  gsender->EMAILBASE64_PASSWORD = rbase64.encode(Gmail_password);
+  gsender->FROM = Gmail_from;
+  if(gsender->Subject(Gmail_subject)->Send(Destination_address, Message)) 
+  {
+    Serial.println("Message is Sent");
+  }
+  else 
+  {
+    Serial.print("Error sending message: ");
+    Serial.println(gsender->getError());
+  }
+}
+
