f046080168f56dc5a96c825f72fdf160873b583d
Aleksander <alekader@rambler.ru>
2018-05-15 19:52:08 +0300

    Update esp8266-telegram-bot-lcd-2004-dist.ino

--- esp8266-telegram-bot-lcd-2004-dist/esp8266-telegram-bot-lcd-2004-dist.ino
+++ esp8266-telegram-bot-lcd-2004-dist/esp8266-telegram-bot-lcd-2004-dist.ino
@@ -28,30 +28,32 @@ LiquidCrystal_I2C lcd(i2c_addres,20,4);  // set the LCD address to 0x27 for a 16
 // UltraSonic connect D6 D5:
 uint8_t triggerPin = D6;
 uint8_t echoPin = D5;
-
 void init_HCSR04(uint8_t triggerPin, uint8_t echoPin)
 {
-    pinMode(triggerPin, OUTPUT);
-    pinMode(echoPin, INPUT);
+  pinMode(triggerPin, OUTPUT);
+  pinMode(echoPin, INPUT);
 }
 
 double measure_HCSR04(void)
 {
-    // Make sure that trigger pin is LOW.
-    digitalWrite(triggerPin, LOW);
-    delayMicroseconds(2);
-    // Hold trigger for 10 microseconds, which is signal for sensor to measure distance.
-    digitalWrite(triggerPin, HIGH);
-    delayMicroseconds(10);
-    digitalWrite(triggerPin, LOW);
-    // Measure the length of echo signal, which is equal to the time needed for sound to go there and back.
-    unsigned long durationMicroSec = pulseIn(echoPin, HIGH);
-    double distanceCm = durationMicroSec / 2.0 * 0.0343;
-    if (distanceCm == 0 || distanceCm > 400) {
-        return -1.0 ;
-    } else {
-        return distanceCm;
-    }
+  // Make sure that trigger pin is LOW.
+  digitalWrite(triggerPin, LOW);
+  delayMicroseconds(2);
+  // Hold trigger for 10 microseconds, which is signal for sensor to measure distance.
+  digitalWrite(triggerPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(triggerPin, LOW);
+  // Measure the length of echo signal, which is equal to the time needed for sound to go there and back.
+  unsigned long durationMicroSec = pulseIn(echoPin, HIGH);
+  double distanceCm = durationMicroSec / 2.0 * 0.0343;
+  if (distanceCm == 0 || distanceCm > 400) 
+  {
+    return -1.0 ;
+  }
+  else 
+  {
+    return distanceCm;
+  }
 }
 
 // Initialize Wifi connection to the router
