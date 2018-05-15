f8d171f4a96283183a9bda8b4dfd512286d42a74
Aleksander <alekader@rambler.ru>
2018-05-15 20:38:15 +0300

    Update esp8266-telegram-bot-lcd-2004-dist.ino

--- esp8266-telegram-bot-lcd-2004-dist/esp8266-telegram-bot-lcd-2004-dist.ino
+++ esp8266-telegram-bot-lcd-2004-dist/esp8266-telegram-bot-lcd-2004-dist.ino
@@ -52,6 +52,34 @@ WiFiClientSecure client;
 UniversalTelegramBot bot(BOTtoken, client);
 DHT dht(DHT_PIN, DHTTYPE);
 
+void init_HCSR04(uint8_t triggerPin, uint8_t echoPin)
+{
+  pinMode(triggerPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+}
+
+double measure_HCSR04(void)
+{
+  // Make sure that trigger pin is LOW.
+  digitalWrite(triggerPin, LOW);
+  delayMicroseconds(2);
+  // Hold trigger for 10 microseconds, which is signal for sensor to measure distance.
+  digitalWrite(triggerPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(triggerPin, LOW);
+  // Measure the length of echo signal, which is equal to the time needed for sound to go there and back.
+  unsigned long durationMicroSec = pulseIn(echoPin, HIGH);
+  double distanceCm = durationMicroSec / 2.0 * 0.0343;
+  if (distanceCm == 0 || distanceCm > 400) 
+  {
+    return -1.0 ;
+  }
+  else 
+  {
+    return distanceCm;
+  }
+}
+
 void sender_message(void)
 {
   String chat_id = String(bot.messages[0].chat_id);
@@ -260,3 +288,4 @@ void loop()
   yield();
   delay(10);
 }
+
